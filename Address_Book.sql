f44f54e7fef074dee76625f7a098b1eeea1be54f
mohdarshad1 <mdarshad181@gmail.com>
2021-05-13 17:00:56 +0530

    [Mohd Arshad] : Add Create Database

--- Address_Book.sql
+++ Address_Book.sql
@@ -1,2 +1,30 @@
 # Welcome to Address Book
 
+#UC1
+
+mysql> CREATE DATABASE AddressBookService;
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| addressbookservice |
+| information_schema |
+| mysql              |
+| payroll_service    |
+| performance_schema |
+| sakila             |
+| sys                |
+| world              |
++--------------------+
+8 rows in set (0.00 sec)
+
+mysql> USE AddressBookService;
+Database changed
+
+mysql> select database();
++--------------------+
+| database()         |
++--------------------+
+| addressbookservice |
++--------------------+
